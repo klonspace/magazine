00f2e0a482a1c8afcc7ad49c821f2e11e5b5fff6
KLON <callumross@bluewin.ch>
2023-12-25 00:50:50 +0100

    more loading checks

--- script.js
+++ script.js
@@ -7,104 +7,112 @@ function playAudio() {
 
 var canvas = document.querySelector("canvas")
 var context = canvas.getContext("2d")
-var img = document.querySelector("img")
-sizeCanvas()
-window.addEventListener("resize", sizeCanvas)
-
-var xmastree = new Image();
-
-xmastree.src = "xmas.png";
+var img = new Image()
 
 
-var drawCanvas = document.createElement("canvas")
-var drawContext = drawCanvas.getContext("2d")
+
+img.onload = () => {
+    document.querySelector("#container").appendChild(img)
+    sizeCanvas()
+    window.addEventListener("resize", sizeCanvas)
+
+    var xmastree = new Image();
+
+    xmastree.src = "xmas.png";
 
 
-drawCanvas.width = window.screen.width;
-drawCanvas.height = window.screen.height;
-
-function sizeCanvas() {
-    var box = img.getBoundingClientRect()
-    canvas.width = box.width
-    canvas.height = box.height
-}
-
-// drawContext.fillStyle = "white"
-
-// drawContext.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
-canvas.addEventListener("mouseenter", startDraw)
-canvas.addEventListener("mousemove", draw)
-canvas.addEventListener("mouseleave", endDraw)
+    var drawCanvas = document.createElement("canvas")
+    var drawContext = drawCanvas.getContext("2d")
 
 
-var drawing = false;
-var lastX = 0, lastY = 0;
+    drawCanvas.width = window.screen.width;
+    drawCanvas.height = window.screen.height;
 
-function drawWrapping() {
-    // context.fillStyle = "yellow"
-    context.drawImage(wrappingPaper, 0, 0, canvas.width, canvas.height)
-}
-function startDraw(e) {
-    drawing = true
-    lastX = e.layerX;
-    lastY = e.layerY;
-    drawShape(e.layerX, e.layerY)
-}
-var xmassize = 40;
-function drawShape(x, y) {
+    function sizeCanvas() {
+        var box = img.getBoundingClientRect()
+        canvas.width = box.width
+        canvas.height = box.height
+    }
 
-    // drawContext.fillStyle = "black"
-    // drawContext.fillRect(x - 5, y - 5, 10, 10)
+    // drawContext.fillStyle = "white"
 
-    drawContext.drawImage(xmastree, x - xmassize / 2, y - xmassize / 2, xmassize, xmassize)
-}
-function draw(e) {
-    // console.log(e)
-    if (drawing) {
-        var dist = Math.sqrt(Math.pow(e.layerX - lastX, 2), Math.pow(e.layerY - lastY, 2));
-        // console.log(dist)
-        for (var i = 0; i < dist; i += 0.3) {
-            drawShape(lastX + ((e.layerX - lastX) * i / dist), lastY + ((e.layerY - lastY) * i / dist))
-        }
+    // drawContext.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
+    canvas.addEventListener("mouseenter", startDraw)
+    canvas.addEventListener("mousemove", draw)
+    canvas.addEventListener("mouseleave", endDraw)
 
-        drawShape(e.layerX, e.layerY)
 
-        context.globalCompositeOperation = "source-over"
-        context.clearRect(0, 0, canvas.width, canvas.height)
-        context.drawImage(drawCanvas, 0, 0)
+    var drawing = false;
+    var lastX = 0, lastY = 0;
 
-        context.globalCompositeOperation = "source-out"
-        drawWrapping();
-
+    function drawWrapping() {
+        // context.fillStyle = "yellow"
+        context.drawImage(wrappingPaper, 0, 0, canvas.width, canvas.height)
+    }
+    function startDraw(e) {
+        drawing = true
         lastX = e.layerX;
         lastY = e.layerY;
+        drawShape(e.layerX, e.layerY)
     }
-}
-function endDraw() {
-    drawing = false;
-}
+    var xmassize = 40;
+    function drawShape(x, y) {
 
+        // drawContext.fillStyle = "black"
+        // drawContext.fillRect(x - 5, y - 5, 10, 10)
 
+        drawContext.drawImage(xmastree, x - xmassize / 2, y - xmassize / 2, xmassize, xmassize)
+    }
+    function draw(e) {
+        // console.log(e)
+        if (drawing) {
+            var dist = Math.sqrt(Math.pow(e.layerX - lastX, 2), Math.pow(e.layerY - lastY, 2));
+            // console.log(dist)
+            for (var i = 0; i < dist; i += 0.3) {
+                drawShape(lastX + ((e.layerX - lastX) * i / dist), lastY + ((e.layerY - lastY) * i / dist))
+            }
 
-var wrappingPaper = new Image()
-wrappingPaper.onload = () => {
-    drawWrapping()
-    document.querySelector("#message").innerHTML = "Click anywhere to start"
-    document.body.addEventListener("click", () => {
-        if (audio.paused) {
-            document.title = "Merry Christmas"
-            audio.play()
-            audio.loop = true;
-            document.querySelector("#message").style.visibility = "hidden"
-            canvas.style.visibility = "visible"
-            setTimeout(() => {
-                img.style.visibility = "visible"
-            }, 100)
+            drawShape(e.layerX, e.layerY)
+
+            context.globalCompositeOperation = "source-over"
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            context.drawImage(drawCanvas, 0, 0)
+
+            context.globalCompositeOperation = "source-out"
+            drawWrapping();
+
+            lastX = e.layerX;
+            lastY = e.layerY;
         }
-    })
+    }
+    function endDraw() {
+        drawing = false;
+    }
+
+
+
+    var wrappingPaper = new Image()
+    wrappingPaper.onload = () => {
+        drawWrapping()
+        document.querySelector("#message").innerHTML = "Click anywhere to start"
+        document.body.addEventListener("click", () => {
+            if (audio.paused) {
+                document.title = "Merry Christmas"
+                audio.play()
+                audio.loop = true;
+                document.querySelector("#message").style.visibility = "hidden"
+                canvas.style.visibility = "visible"
+                setTimeout(() => {
+                    img.style.visibility = "visible"
+                }, 100)
+            }
+        })
+    }
+
+    wrappingPaper.src = "cover2.jpg";
 }
 
-wrappingPaper.src = "cover2.jpg";
+img.src = "no2.jpg"
 
 
 
